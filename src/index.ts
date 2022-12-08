695c570d42a9c6991ee253ec4bd8c3f4cc45f3e8
Shohei Umemoto <cafedomancer@gmail.com>
2022-12-08 14:43:02 +0900

    Validate JWTs

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,33 @@
-import { Hono } from 'hono'
-const app = new Hono()
+import { Hono } from "hono";
+import * as jose from "jose";
 
-app.get('/', (c) => c.text('Hello! Hono!'))
+const app = new Hono();
 
-export default app
+const TEAM_DOMAIN = "cafedomancer"; // TODO your team domain
+const POLICY_AUD =
+  "b6f01f510a3ee259afc887bfe593b889a37e6f36663ced575c23a54b890f556d"; // TODO your policy aud
+
+// TODO extract as middleware
+
+const JWKS = jose.createRemoteJWKSet(
+  new URL(`https://${TEAM_DOMAIN}.cloudflareaccess.com/cdn-cgi/access/certs`)
+);
+
+app.get("/", async (c) => {
+  const jwt = c.req.headers.get("Cf-Access-Jwt-Assertion");
+  if (!jwt) return c.status(401);
+
+  const { payload } = await jose.jwtVerify(jwt, JWKS, {
+    algorithms: ["RS256"],
+    audience: POLICY_AUD,
+    issuer: `https://${TEAM_DOMAIN}.cloudflareaccess.com`,
+  });
+
+  console.log(payload);
+
+  const email = payload.email;
+
+  return c.json({ message: `hello, ${email}` });
+});
+
+export default app;
